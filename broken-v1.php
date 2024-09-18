62f65e40246d2ffa7d66c4bcd13f9635af0a42a3
unknown <your@email.example>
2024-09-18 10:42:37 +0300

    fixed

--- broken-v1.php
+++ broken-v1.php
@@ -18,22 +18,25 @@ $books = [
 
 function showAllBooks($books) {
     foreach ($books as $id => $book) {
-        // need to display each book here
+        displayBook($id, $book);
     }
 }
 
-function showBook() {
+function showBook($books) {
     $id = readline("Enter book id: ");
     displayBook($id, $books[$id]);
 }
 
 function addBook(&$books) {
     $title = readline("Enter title: ");
+    $author = readline("Enter author:");
+    $id = count($books) + 1;
     $books[] = ['title' => $title, 'author' => $author];
 }
 
 function deleteBook(&$books) {
     $id = readline("Enter book ID you want to delete: ");
+    unset($books[$id]);
 }
 
 function displayBook($id, $book) {
